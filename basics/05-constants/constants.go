2d0bd53ccc70c9f872e45fba32803d5e98f3c524
Alexander <a.nasulchak@gmail.com>
2026-02-06 23:15:49 +0200

    Train arithmetic operators, foor loop, for loop as while, make guessing game

---
+++ basics/05-constants/constants.go
@@ -0,0 +1,15 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.81
+
+func main() {
+	const DAYS int = 7
+
+	const (
+		MONDAY      = 1
+		TUESDAT     = 2
+		WENEDAY     = 3
+		TRUSDAY int = 4
+	)
+}
---
+++ basics/08-for_as_while/for_as_while.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	// for as while
+	i := 1
+	for i <= 5 {
+		fmt.Println("Iteration:", i)
+		i++
+	}
+
+	// for as while with break
+	sum := 0
+	for {
+		sum += 10
+		fmt.Println("Sum:", sum)
+		if sum >= 50 {
+			break
+		}
+	}
+
+	// for as while with continue
+	num := 1
+	for num <= 10 {
+		if num%2 == 0 {
+			num++
+			continue
+		}
+		fmt.Println("Odd Number:", num)
+		num++ // ++ increment operator increeases value bu 1 and -- decrement operator, decreases value by 1
+	}
+
+}
