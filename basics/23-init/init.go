0dd9609079dd4069c91fa3f700f784a4e0843209
Alexander <a.nasulchak@gmail.com>
2026-02-13 22:20:33 +0200

    Init function

---
+++ basics/23-init/init.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("Initializind package1...")
+}
+
+func init() {
+	fmt.Println("Initializind package2...")
+}
+
+func init() {
+	fmt.Println("Initializind package3...")
+}
+
+func main() {
+
+	fmt.Println("Inside the main function")
+
+}
