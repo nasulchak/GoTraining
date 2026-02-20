9d6b8c53d139d8f0a01e26ded683fb90b682d47f
Alexander <a.nasulchak@gmail.com>
2026-02-20 21:37:20 +0200

    Interfaces

---
+++ intermediate/09-interfaces/interfaces.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func (r rect) perim() float64 {
+	return 2 * (r.height + r.width)
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func (c circle) diameter() float64 {
+	return 2 * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
+func main() {
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+
+	measure(r)
+	measure(c)
+
+	myPrinter(1, "John", 45.9, true)
+	// printType(8)
+	// printType("John")
+	// printType(false)
+}
+
+func printType(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("Type: Int")
+	case string:
+		fmt.Println("Type: String")
+	default:
+		fmt.Println("Type: Unknown")
+	}
+}
+
+func myPrinter(i ...interface{}) {
+	for _, v := range i {
+		fmt.Println(v)
+		printType(v)
+	}
+}
