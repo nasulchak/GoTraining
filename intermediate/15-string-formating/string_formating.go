97ae6474b7ef9b2d6ca431ab1783f4e79321f7b5
Alexander <a.nasulchak@gmail.com>
2026-02-25 21:39:54 +0200

    String formating

---
+++ intermediate/15-string-formating/string_formating.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	num := 42
+	fmt.Printf("%05d\n", num)
+
+	message := "Hello"
+	fmt.Printf("|%10s|\n", message)
+	fmt.Printf("|%-10s|\n", message)
+
+	message1 := "Hello \nWorld!"
+	message2 := `Hello \nWorld!`
+	fmt.Println(message1)
+	fmt.Println(message2)
+
+}
