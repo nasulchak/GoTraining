1e6ed78a12b9a7745f8bc91f34dc7528e3a347f5
Alexander <a.nasulchak@gmail.com>
2026-03-14 19:46:19 +0200

    Line filters

---
+++ intermediate/29-line-filter/line_filter.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	file, err := os.Open("example.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// Keyword to filter lines
+	keyword := "important"
+
+	lineNumber := 1
+
+	// Read an filter lines
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, keyword) {
+			updatedLine := strings.ReplaceAll(line, keyword, "necessaty")
+			fmt.Printf("%d.Filtered line: %v\n", lineNumber, line)
+			fmt.Printf("%d.Updated line: %v\n", lineNumber, updatedLine)
+		}
+		lineNumber++
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+}
