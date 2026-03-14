9b4de3a30a842dc5b896a51cad5b507f1647919e
Alexander <a.nasulchak@gmail.com>
2026-03-14 13:42:28 +0200

    Writing Files

---
+++ intermediate/27-writing-files/writing_files.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Write data to file
+	data := []byte("Hello World!\n")
+
+	_, err = file.Write(data)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Data has been written to file success!")
+
+	file, err = os.Create("writeString.txt")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("Hello Go!\n")
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("Writing to writeString.txt complete")
+
+}
