08f38032a3c709508a3e7f8f69af4d684bed11d1
Alexander <a.nasulchak@gmail.com>
2026-03-16 20:56:04 +0200

    Temporary files and dirs

---
+++ intermediate/32-temp-files-dirs/temp_files_dirs.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	tempFileName := "temporaryFile"
+	tempFile, err := os.CreateTemp("", tempFileName)
+	checkError(err)
+
+	fmt.Println("Temporary file created:", tempFile.Name())
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
+
+	tempDir, err := os.MkdirTemp("", "GoCourseTempDir")
+	checkError(err)
+
+	defer os.Remove(tempDir)
+
+	fmt.Println("Temporary directory created:", tempDir)
+}
