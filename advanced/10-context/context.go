8459eed82504f2f0b7619dc482b842d74dde5138
Alexander <a.nasulchak@gmail.com>
2026-04-15 21:59:30 +0300

    Context

--- advanced/10-context/context.go
+++ advanced/10-context/context.go
@@ -6,36 +6,69 @@ import (
 	"time"
 )
 
-func checkEvenOdd(ctx context.Context, num int) string {
-	select {
-	case <-ctx.Done():
-		return "Operration canceled"
-	default:
-		if num%2 == 0 {
-			return fmt.Sprintf("%d is even", num)
-		} else {
-			return fmt.Sprintf("%d is odd", num)
+func doWork(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Work cancelled:", ctx.Err())
+			return
+		default:
+			fmt.Println("Working...")
 		}
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
 func main() {
-	ctx := context.TODO()
+	rootCtx := context.Background()
 
-	result := checkEvenOdd(ctx, 5)
-	fmt.Println("Result with context.TODO():", result)
-
-	ctx = context.Background()
-	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	ctx, cancel := context.WithTimeout(rootCtx, 2*time.Second)
 	defer cancel()
 
-	result = checkEvenOdd(ctx, 10)
-	fmt.Println("Result from timeout context:", result)
+	ctx = context.WithValue(ctx, "requesID", "sadawser32543rfwe")
 
-	time.Sleep(2 * time.Second)
-	result = checkEvenOdd(ctx, 15)
-	fmt.Println("Result after timeout:", result)
+	go doWork(ctx)
+
+	time.Sleep(3 * time.Second)
+
+	requestID := ctx.Value("requesID")
+	if requestID != nil {
+		fmt.Println("Request ID:", requestID)
+	} else {
+		fmt.Println("No requestID found")
+	}
 }
+
+// func checkEvenOdd(ctx context.Context, num int) string {
+// 	select {
+// 	case <-ctx.Done():
+// 		return "Operration canceled"
+// 	default:
+// 		if num%2 == 0 {
+// 			return fmt.Sprintf("%d is even", num)
+// 		} else {
+// 			return fmt.Sprintf("%d is odd", num)
+// 		}
+// 	}
+// }
+
+// func main() {
+// 	ctx := context.TODO()
+
+// 	result := checkEvenOdd(ctx, 5)
+// 	fmt.Println("Result with context.TODO():", result)
+
+// 	ctx = context.Background()
+// 	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+// 	defer cancel()
+
+// 	result = checkEvenOdd(ctx, 10)
+// 	fmt.Println("Result from timeout context:", result)
+
+// 	time.Sleep(2 * time.Second)
+// 	result = checkEvenOdd(ctx, 15)
+// 	fmt.Println("Result after timeout:", result)
+// }
 
 // ====== DIFFERENCE BETWEEN CONTEXT.TOD AND CONTEXT.BACKGROUND
 // func main() {
